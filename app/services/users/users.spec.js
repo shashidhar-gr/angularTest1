75c77d4b15411ca415066169990fc07d229e3485
shashidhar-gr <shashidhargr26@gmail.com>
2017-01-17 12:06:21 +0530

    Added all

---
+++ app/services/users/users.spec.js
@@ -0,0 +1,9 @@
+describe('Users service testing-->', function() {
+	it('Users data testing 1', function() {
+		expect(3).toEqual(3);
+	})
+
+	it('Users data testing 2', function() {
+		expect(3).toEqual(3);
+	})
+})
--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
-app.listen(3000, function() {
+app.listen(8080, function() {
 	console.log("express app listening on port 3000.");
 })
